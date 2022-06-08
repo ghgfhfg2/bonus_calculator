8e2257aa8fca8f7c9aed4c80213b74b910ac26cc
hyunsoo kim <ghgfhfg2@naver.com>
2022-06-08 16:14:54 +0900

    .

--- js/bonus.js
+++ js/bonus.js
@@ -1,3 +1,7 @@
+const onHiddenCon = (e) => {
+  e.parentElement.classList.toggle('on')
+}
+
   let sudang_3_table = [
       {
         idx:1,
@@ -196,13 +200,7 @@
   let sudang_5;
   let sudang_6;
 
-  let result_1 = document.querySelector('.sudang__calc_result_1');
-  let result_2 = document.querySelector('.sudang__calc_result_2');
-  let result_3 = document.querySelector('.sudang__calc_result_3');
-  let result_4 = document.querySelector('.sudang__calc_result_4');
-  let result_5 = document.querySelector('.sudang__calc_result_5');
-  let result_6 = document.querySelector('.sudang__calc_result_6');
-  let result_total = document.querySelector('.sudang__calc_result_total');
+  //let result_total = document.querySelector('.sudang__calc_result_total');
   let result_box = document.querySelector('.sudang__calc_result');  
   
   let result_1_info = document.querySelector('.sudang__calc_result_1_info');
@@ -217,10 +215,19 @@
   const bonusInfo = (e) => {
     let listAll = document.querySelectorAll('.sudang__calc_result_list_li');
     let check = e.parentElement.classList.contains('on');
-    listAll.forEach(el=>el.classList.remove('on'))
+    listAll.forEach(el=>el.classList.remove('on'));
     if(!check){
-      e.parentElement.classList.add('on')
+      e.parentElement.classList.add('on');
     }
+  }
+
+  const clearResult = () => {
+    let dt = document.querySelectorAll('.sudang__calc_result_info dt')
+    let price = document.querySelectorAll('.sudang__calc_result_info .sudang_price')
+    let tbl = document.querySelectorAll('.sudang__calc_result_info_table')
+    dt.forEach(el=>el.innerHTML = '')
+    price.forEach(el=>el.innerHTML = '')
+    //tbl.forEach(el=>el.querySelector('tbody').innerHTML = '')
   }
   
   const clacClear = () => {
@@ -238,17 +245,20 @@
     document.querySelector('.sudang__calc_format_price.sales1_price').innerHTML = '0만원';
     document.querySelector('.sudang__calc_format_price.sales2_price').innerHTML = '0만원';
 
-    result_1.innerHTML = '';
-    result_2.innerHTML = '';
-    result_3.innerHTML = '';
-    result_4.innerHTML = '';
-    result_5.innerHTML = '';
-    result_6.innerHTML = '';
-    result_total.innerHTML = '';    
+    clearResult();
     result_box.classList.remove('on')
   }
 
+  const maxCheck = (e) => {
+    if(e.value > e.max){
+      e.value = e.max;
+    }
+  }
+
   const onPriceFormat = (price,div) => {
+    if(price.value[0] == 0){
+      price.value = price.value.substr(1)
+    }
     let format_price = price.value;
     if(format_price.length > 8){
       price.value = price.value.substr(0,8);
@@ -258,12 +268,12 @@
     let uk;
     let man;
     if(format_price.length <= 4){
-      price_txt = format_price + '만원';
+      price_txt = commaNumber(format_price) + '만원';
     }else {
       uk = Math.floor(format_price/10000);
-      uk = uk ? uk+'억' : '';
+      uk = uk ? commaNumber(uk)+'억' : '';
       man = format_price % 10000;
-      man = man ? man+'만' : '';
+      man = man ? commaNumber(man)+'만' : '';
       price_txt = `${uk} ${man}원`;
     }
 
@@ -279,37 +289,65 @@
     let price_txt;
     let uk;
     let man;
+    price = price ? price : 0;
     if(price.length <= 4){
-      price_txt = price + '만원'
+      price_txt = `<span>${price}</span>` + '만원'
     }else{
       uk = Math.floor(price/10000);
-      uk = uk ? uk+'억' : '';
-      man = price % 10000;
-      man = man >= 0.1 ? man+'만' : '';
-      price_txt = `${uk} ${man}원`
+      uk = uk ? `<span>${commaNumber(uk)}</span>`+'억' : '';
+      man = Math.round(price % 10000);
+      man = man > 0 ? `<span>${commaNumber(man)}</span>`+'만' : '';
+      price_txt = `${uk}${man}원`
     }
     return price_txt;
   }
 
-  const calcPromotionSudang = (total,sudang1) => {
+  const calcPromotionSudang = (total,sudang1,bunus) => {
     let sudang_5 = 0;
+    let list5 = document.querySelector('.sudang__calc_result_info.list_5');
+    let perDt = list5.querySelector('.calc_per dt');
+    let tbl = document.querySelector('.sudang__calc_result_info_table.tbl_3')
+    let per;
+    let price = `${onPriceFormat2(sudang1)}`;
+    price += `<span class="ic_x small"></span>`;
+    price += `<span>${bunus}</span> %`;
+    list5.querySelector('.calc_price dt').innerHTML = price;
+
+    tbl.querySelectorAll('tbody tr').forEach(el=>{
+      el.classList.remove('on');
+    })
     if(total < 200){
-      sudang_5 = sudang1;
+      sudang_5 = sudang1*bunus/100;
+      per = `<span>100</span> %`;
+      tbl.querySelector('tbody tr:nth-child(1)').classList.add('on')
     }
     if(total >= 200 && total < 400){
-      sudang_5 = sudang1*0.5
+      sudang_5 = sudang1*bunus/100*0.5;
+      per = `<span>50</span> %`;
+      tbl.querySelector('tbody tr:nth-child(2)').classList.add('on')
     }
     if(total >= 400 && total < 500){
-      sudang_5 = sudang1*0.3
+      sudang_5 = sudang1*bunus/100*0.3;
+      per = `<span>30</span> %`;
+      tbl.querySelector('tbody tr:nth-child(3)').classList.add('on')
     }
+    if(total >= 500){
+      per = `<span>0</span> %`;
+    }
+    perDt.innerHTML = per;
     sudang_5 = sudang_5 > 30 ? 30 : sudang_5;
+    list5.querySelector('.sudang_price').innerHTML = sudang_5 ? `${onPriceFormat2(sudang_5)}` : `<span>0</span>원`;
     return sudang_5;
   }
 
   const calcPromotionSudang2 = (total,sudang_mat) => {
     let sudang_6 = 0;
+    let tbl = document.querySelector('.sudang__calc_result_info_table.tbl_4 tbody tr');
     if(total < 1000){
       sudang_6 = sudang_mat;
+      tbl.classList.add('on');
+    }else{
+      tbl.classList.remove('on');
     }
     sudang_6 = sudang_6 > 20 ? 20 : sudang_6;
     return sudang_6;
@@ -336,9 +374,9 @@
     let priceBox = divBox.querySelector('.calc_price dt')
     let perBox = divBox.querySelector('.calc_per dt')
     let sudangBox = divBox.querySelector('.sudang_price')
-    priceBox.innerHTML = price + '만원'
-    perBox.innerHTML = per + '%'
-    sudangBox.innerHTML = price * per/100 + '만원'
+    priceBox.innerHTML = onPriceFormat2(price);
+    perBox.innerHTML = `<span>${per}</span>` + '%'
+    sudangBox.innerHTML = onPriceFormat2(price * per/100);
   }
 
   const calcInfo2 = (table,div,id) => {
@@ -364,6 +402,25 @@
     })
     divBox.querySelector('tbody').innerHTML = tbody;
     
+  }
+
+  const tableInfo = (table,div) => {
+    let divBox = document.querySelector(div);
+    let tbody = '';
+    table.forEach(el=>{
+      let price = onPriceFormat2(el.price)
+      let bonus = onPriceFormat2(el.bonus)
+      let tr = '';
+      tr += `<tr>`;
+      tr += `<td>${price}</td>`;
+      if(el.rank){
+        tr+= `<td>${el.rank}</td>`;  
+      }  
+      tr += `<td>${bonus}</td>`;
+      tr += `</tr>`;
+      tbody += tr;
+    })
+    divBox.querySelector('tbody').innerHTML = tbody;    
   }
 
   let clacSubmit = ()=>{
@@ -393,7 +450,7 @@
       f.bonus_2.focus();
       return;
     }
-
+    clearResult();
     let total_sales = parseInt(sales_1.value) + parseInt(sales_2.value);
 
     sudang_1 = sales_1.value * f.bonus_1.value/100;
@@ -401,48 +458,75 @@
     sudang_mat = sales_2.value * f.bonus_1.value/100;
     sudang_3 = calcSudang(sudang_3_table,total_sales);
     sudang_4 = calcSudang(sudang_4_table,total_sales);
-    sudang_5 = calcPromotionSudang(total_sales,sudang_1);
+    sudang_5 = calcPromotionSudang(total_sales,sales_1.value,bonus_1.value);
     sudang_6 = calcPromotionSudang2(total_sales,sudang_mat);
 
 
     calcInfo(sales_1.value,f.bonus_1.value,'.sudang__calc_result_info.list_1')
     calcInfo(sales_2.value,f.bonus_2.value,'.sudang__calc_result_info.list_2')
-    if(sudang_5){
-      calcInfo(sales_1.value,f.bonus_1.value,'.sudang__calc_result_info.list_3')
-    }else{
-      document.querySelector('.sudang__calc_result_info.list_3').classList.add('no')
-    }
-    if(sudang_6){
-      calcInfo(sales_2.value,f.bonus_1.value,'.sudang__calc_result_info.list_4')
-    }else{
-      document.querySelector('.sudang__calc_result_info.list_4').classList.add('no')
-    }
+    
     
     calcInfo2(sudang_3_table,'.sudang__calc_result_info_table.tbl_1',sudang_3.idx)
     calcInfo2(sudang_4_table,'.sudang__calc_result_info_table.tbl_2',sudang_4.idx)
 
 
+
+    calcInfo(sales_2.value,f.bonus_1.value,'.sudang__calc_result_info.list_6')    
+
+
+
+
     let sudang_total = (sudang_1 + sudang_2 + sudang_3.bonus + sudang_4.bonus + sudang_5 +sudang_6).toFixed(2);
-    sudang_total = onPriceFormat2(sudang_total)
+    sudang_total = onPriceFormat2(sudang_total);
 
     sudang_1 = sudang_1 ? `${onPriceFormat2(sudang_1)}` : '0만원';
-    sudang_2 = sudang_2 ? `${sudang_2}만원` : '0만원';
-    sudang_3 = `${sudang_3.bonus}만원`;
-    sudang_4 = sudang_4.rank ? `${sudang_4.rank} - ${sudang_4.bonus}만원` 
-                : `${sudang_4.bonus}만원`;
+    sudang_2 = sudang_2 ? `${onPriceFormat2(sudang_2)}` : '0만원';
+    sudang_3 = `${onPriceFormat2(sudang_3.bonus)}`;
+
     sudang_5 = `${sudang_5}만원`;
     sudang_6 = `${sudang_6}만원`;
     
-    result_1.innerHTML = sudang_1;
-    result_2.innerHTML = sudang_2;
-    result_3.innerHTML = sudang_3;
-    result_4.innerHTML = sudang_4;
-    result_5.innerHTML = sudang_5;
-    result_6.innerHTML = sudang_6;
-    result_total.innerHTML = sudang_total;
+    
+    //result_total.innerHTML = sudang_total;
 
+   //장려보너스
+   let sudangBox3 = document.querySelector('.sudang__calc_result_info.list_3');
+   sudangBox3.querySelector('.calc_price dt').innerHTML = `${onPriceFormat2(total_sales)}`;
+   sudangBox3.querySelector('.sudang_price').innerHTML = sudang_3;
+
+   //직급보너스
+   let sudangBox4 = document.querySelector('.sudang__calc_result_info.list_4');  
+   sudangBox4.querySelector('.calc_price dt').innerHTML = `${onPriceFormat2(total_sales)} /&nbsp;<span class="rank">${sudang_4.rank}</span>`;
+   sudangBox4.querySelector('.sudang_price').innerHTML = onPriceFormat2(sudang_4.bonus); 
 
 
     result_box.classList.add('on')
 
+  }
+
+  const commaNumber = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+
+  //레이어팝업 
+  const onLayer = async (div,url,type) => {
+    let box = document.querySelector(div);
+    let con = box.querySelector('.sudang__calc_layer_con')
+    let html = await fetch(url)
+    .then(res=>res.text())
+    .then(res=>res)
+    con.innerHTML = html;
+    if(type === 1){
+      tableInfo(sudang_3_table,'.sudang__calc_layer_desc .sudang__calc_result_info_table')
+    }
+    if(type === 2){
+      tableInfo(sudang_4_table,'.sudang__calc_layer_desc .sudang__calc_result_info_table')
+    }
+    box.classList.add('on');
+    document.body.classList.add('fix');
+  }
+  const closeLayer = (div) => {
+    document.querySelector(div).classList.remove('on')
+    document.body.classList.remove('fix');
   }
